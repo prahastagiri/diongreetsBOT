a0cfac987f2155a9e09eaf92d7be33d591e314f5
prahastagiri <adiprahastagiri18@gmail.com>
2020-09-26 16:16:09 +0800

    hide bot token

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json');
 
-const prefix = '#';
+const prefix = config.prefix;
 
 const fs = require('fs');
  
@@ -107,4 +108,4 @@ client.on('message',(message)=>{
     }
 })
 
-client.login('NzU5MDcyMTUyNzgzMjkwMzY4.X24LCA.Wm7ATJR6lzS-zrvSXGVLCT92S58')
+client.login(config.token)
